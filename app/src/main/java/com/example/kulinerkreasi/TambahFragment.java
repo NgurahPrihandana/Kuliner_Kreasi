d034dcb55f050cfc69f197f73a9c57d0eae8f251
NgurahPrihandana <prihandana27@gmail.com>
2023-07-01 20:31:33 +0800

    Fix Error

--- app/src/main/java/com/example/kulinerkreasi/TambahFragment.java
+++ app/src/main/java/com/example/kulinerkreasi/TambahFragment.java
@@ -1,5 +1,6 @@
 package com.example.kulinerkreasi;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,43 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class TambahFragment extends Fragment {
+    LinearLayout tambahResep;
+    LinearLayout tambahBerita;
+
     @Nullable
     @Override
     public View onCreateView( LayoutInflater inflater,@Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tambah, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_tambah, container, false);
+
+        tambahResep = rootView.findViewById(R.id.opsi_tambah_resep);
+        tambahBerita = rootView.findViewById(R.id.opsi_tambah_berita);
+
+        tambahResep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        tambahBerita.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        return rootView;
+
     }
+
+
+
 }
